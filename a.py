ca20391c6ae0c60cce1beb136ab6d06530001099
sujithmalga <sujithmalga@gmail.com>
2024-08-27 16:43:43 +0530

    msg

--- a.py
+++ a.py
@@ -1,1 +1,10 @@
-cp multithreading.py ./python/
+def prime(n):
+    if n <= 1:
+        print("it is not  prime")
+    for i in range(2,int(n**0.5) + 1):
+        if n%i == 0:
+            print("it not prime number")
+    return "prime number"
+        
+n = int(input("enter the number"))
+print(prime(n))
