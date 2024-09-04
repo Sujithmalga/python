782c1480bd0b1005bd7800f839e01dadf9d74392
Sujithmalga <sujithmalga@gmail.com>
2024-09-04 20:57:11 +0530

    new

--- a.py
+++ a.py
@@ -1,10 +1,10 @@
 def prime(n):
     if n <= 1:
-        print("not  prime")
+        print("not  ax prime")
     for i in range(2,int(n**0.5) + 1):
         if n%i == 0:
             print("not prime number")
     return "prime "
         
-n = int(input("enter the number"))
+n = int(input("enter the number you want"))
 print(prime(n))
