daaa3572c62a8bc4042a455708f5ba284fce65e7
Sujithmalga <sujithmalga@gmail.com>
2024-09-06 11:44:43 +0530

    Revert "new"
    This reverts commit 782c1480bd0b1005bd7800f839e01dadf9d74392.

--- a.py
+++ a.py
@@ -1,10 +1,10 @@
 def prime(n):
     if n <= 1:
-        print("not  ax prime")
+        print("not  prime")
     for i in range(2,int(n**0.5) + 1):
         if n%i == 0:
             print("not prime number")
     return "prime "
         
-n = int(input("enter the number you want"))
+n = int(input("enter the number"))
 print(prime(n))
