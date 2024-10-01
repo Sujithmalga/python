f9e550ac9f0b114b488883a9d087c00c180d2da1
Sujithmalga <109858061+Sujithmalga@users.noreply.github.com>
2024-10-01 13:10:10 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='sujith',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your project dependencies here
+        # 'requests', 'numpy', etc.
+    ],
+    entry_points={
+        'console_scripts': [
+            'your-script-name=your_package.module:first build',  # For creating command-line tools
+        ],
+    },
+)
